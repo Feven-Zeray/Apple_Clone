77978c5d83855cf05652c74f71bd7b0fb8dab9db
Feven-Zeray <fevenzeray2028@gmail.com>
2025-04-14 04:31:20 -0500

    errorfixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Apple-Clone/">
+    <BrowserRouter basename="/Apple_Clone/">
       <App />
     </BrowserRouter>
   </StrictMode>
